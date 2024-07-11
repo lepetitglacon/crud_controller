18b917bbd033f291271f94e2a8cb0065806b0a06
petitglacon <estebangagneur03@gmail.com>
2024-07-11 07:41:23 +0200

    [TASK] update ext_emconf.php
    (cherry picked from commit 5a66c27bc560f920a26c3b1ab2989fd1ed90830d)

---
+++ ext_emconf.php
@@ -0,0 +1,20 @@
+<?php
+
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'CRUD Controller',
+    'description' => 'An abstract CRUD controller for extbase domain models',
+    'category' => 'plugin',
+    'author' => 'petitglacon',
+    'author_email' => 'estebangagneur03@gmail.com',
+    'state' => 'alpha',
+    'version' => '0.1.0',
+    'constraints' => [
+        'depends' => [
+            'typo3' => '12.0.0-12.4.99',
+        ],
+        'conflicts' => [
+        ],
+        'suggests' => [
+        ],
+    ],
+];
